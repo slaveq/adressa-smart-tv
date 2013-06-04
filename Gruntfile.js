8d905ab738d8518ac645fda775fb8203c4237a5b
Adam Lukaszczyk <dobiatowski@gmail.com>
2013-06-04 17:36:58 +0200

    Builder compiling requirejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,44 +3,64 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: ["build/"],
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['js/**/*.js'],
-                dest: 'build/<%= pkg.name %>.js'
-            },
-            css: {
-                src: 'css/*.css',
-                dest: 'build/style.css'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
-            dist: {
-                files: {
-                    'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: "scripts",
+                    name: "app",
+                    mainConfigFile: "scripts/app.js",
+                    out: "build/script.js"
                 }
             }
         },
+//        concat: {
+//            options: {
+//                separator: ';'
+//            },
+//            dist: {
+//                src: ['js/**/*.js'],
+//                dest: 'build/<%= pkg.name %>.js'
+//            },
+//            css: {
+//                src: 'css/*.css',
+//                dest: 'build/style.css'
+//            }
+//        },
+//        uglify: {
+//            options: {
+//                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+//            },
+//            dist: {
+//                files: {
+//                    'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+//                }
+//            }
+//        },
         copy: {
             main: {
                 files: [
-                    {expand: true, src: ['./config.xml', './widget.info', './index.html'], dest: 'build/', filter: 'isFile'}, // includes files in path
+                    {expand: true, src: ['./config.xml', './widget.info'], dest: 'build/', filter: 'isFile'}, // includes files in path
+                    {expand: true, src: ['mocks/**'], dest: 'build/'}, // includes files in path and its subdirs
+                    {expand: true, src: ['scripts/libs/**'], dest: 'build/'}, // includes files in path and its subdirs
+                    {expand: true, src: ['css/**.css'], dest: 'build/'}, // includes files in path and its subdirs
                     {expand: true, src: ['images/**'], dest: 'build/'} // includes files in path and its subdirs
                 ]
+            }
+        },
+        targethtml: {
+            dist: {
+                files: {
+                    'build/index.html': 'index.html'
+                }
             }
         },
         compress: {
             main: {
                 options: {
-                    archive: 'archive.zip'
+                    archive: 'build/archive.zip'
                 },
                 files: [
-                    {src: ['build/**'], dest: '/', filter: 'isFile'} // includes files in path
+                    {expand: true, cwd: 'build/', src: ['**'], dest: ''} // includes files in path
                 ]
             }
         }
@@ -52,7 +72,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-targethtml');
 
-    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'copy', 'compress']);
+//    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'copy', 'compress']);
+    grunt.registerTask('default', ['clean', 'requirejs', 'copy', 'targethtml:dist', 'compress']);
 
 };
